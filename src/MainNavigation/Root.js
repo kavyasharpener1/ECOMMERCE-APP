0df6ef59aeae5a937b67caac7cea5a5580f9ef13
kavyasharpener1 <naikkavya201@gmail.com>
2023-09-08 12:58:01 +0530

    deploy

---
+++ src/MainNavigation/Root.js
@@ -0,0 +1,14 @@
+import React from "react";
+import WelCome from "./WelCome";
+// import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+
+const Root = (props) => {
+    return (<>
+        <WelCome />
+        <main>
+            {props.children}
+        </main>
+    </>
+    )
+}
+export default Root;
